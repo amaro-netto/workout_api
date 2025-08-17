168c1950fdb11d25f4d048ae6b260bc08669ba53
amaro-netto <amarosilva002@gmail.com>
2025-08-17 20:38:41 -0300

    feat: Adiciona o schema AtletaResumo para paginação e resposta customizada

--- module/atleta/schemas.py
+++ module/atleta/schemas.py
@@ -22,3 +22,8 @@ class AtletaOut(OutMixin):
 class AtletaUpdate(BaseSchema):
     nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
     idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
+
+class AtletaResumo(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
+    centro_treinamento: Annotated[str, Field(description='Centro de treinamento do atleta', example='CT King', max_length=20)]
+    categoria: Annotated[str, Field(description='Categoria do atleta', example='Scale', max_length=10)]
