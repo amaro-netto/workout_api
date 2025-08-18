c7ff997a4bf74c272e0ac368bb91c3cdd7b6eeb2
amaro-netto <amarosilva002@gmail.com>
2025-08-18 19:40:35 -0300

    fix: Corrige erro de importação do objeto 'context' no Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -35,14 +35,15 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+
 def do_run_migrations(connection: Connection) -> None:
     context.configure(connection=connection, target_metadata=target_metadata)
     
     with context.begin_transaction():
         context.run_migrations()
 
+
 async def run_async_migrations() -> None:
-    # AQUI ESTÁ A MUDANÇA IMPORTANTE
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -52,10 +53,11 @@ async def run_async_migrations() -> None:
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
 
+
 def run_migrations_online() -> None:
-    # E AQUI TAMBÉM
     config.set_main_option("sqlalchemy.url", settings.DB_URL)
     asyncio.run(run_async_migrations())
+
 
 if context.is_offline_mode():
     run_migrations_offline()
